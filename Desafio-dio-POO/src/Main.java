d179403877564fe1bfc808617eea1fe632200994
GeovannaLemos <geovannalv2@gmail.com>
2025-03-03 21:32:13 -0300

    desafio dio POO

--- Desafio-dio-POO/src/Main.java
+++ Desafio-dio-POO/src/Main.java
@@ -1,4 +1,6 @@
+import desafioDominio.Bootcamp;
 import desafioDominio.Curso;
+import desafioDominio.Dev;
 import desafioDominio.Mentoria;
 import java.time.LocalDate;
 
@@ -21,8 +23,38 @@ public class Main {
         mentoria1.setDescricao("descrição mentoria java");
         mentoria1.setData (LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria1);
+        System.out.println(mentoria1);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Camila: "+devCamila.getConteudosInscritos());
+        devCamila.progredir();
+        System.out.println("-----");
+        System.out.println("Conteúdos inscritos Camila: "+devCamila.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Camila: "+devCamila.getConteudosInscritos());
+        System.out.println("XP: "+devCamila.calcularTotlXp());
+
+        System.out.println("\t-------\t");
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("João");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Joao: "+devJoao.getConteudosInscritos());
+        System.out.println("-----");
+        devJoao.progredir();
+        System.out.println("Conteúdos inscritos Joao: "+devJoao.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Joao: "+devJoao.getConteudosInscritos());
+        System.out.println("XP: "+devJoao.calcularTotlXp());
+
     }
 }
